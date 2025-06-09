6174b8559989f096d5f095c1dc5e8fdc3fc10281
Antonio Fernandez <tfernandez.sc2@gmail.com>
2025-06-09 16:27:10 -0400

    Initial commit

---
+++ dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:latest
+
+WORKDIR /tmp
+
+RUN echo "$HOSTNAME" > /etc/hostname
+
+RUN apt-get update && \
+    apt-get install -y \
+        bash \
+        xz-utils \
+        ca-certificates \
+        tzdata \
+        libstdc++6 \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY ./Comet_Backup-25.5.0.run ./
+COPY ./entrypoint.sh /
+RUN chmod +x /entrypoint.sh
+
+CMD ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Verify if the Comet Backup folder exists
+if [ -d /opt/CometBackup ]; then
+    echo "/opt/CometBackup is not empty"
+    if [ ! -f /root/.config/backup-tool/config.dat ]; then
+        echo "/root/.config/backup-tool/config.dat is missing!"
+    else
+        /opt/CometBackup/backup-daemon-start.sh
+    fi
+    
+else
+    echo "/opt/CometBackup is empty"
+    /tmp/Comet_Backup-25.5.0.run --lobby
+fi
+
+while true; do
+    if ! pgrep -f backup-tool > /dev/null; then
+        echo "backup-tool service is not running!"
+        break
+    fi
+    sleep 5
+done
